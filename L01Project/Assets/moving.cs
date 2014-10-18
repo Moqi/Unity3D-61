0714629a52267e23e10615de3c0f06e5bf0b7dff
Ilian Kostov <ilian.kostov@gmail.com>
2014-10-18 15:18:31 +0300

    Unity 3D Practice

---
+++ L01Project/Assets/moving.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moving : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.Translate( 0f, 3f*Time.deltaTime, 0f );
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.Translate( 0f, -3f*Time.deltaTime, 0f );
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate( -3f*Time.deltaTime, 0f, 0f );
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Translate( 3f*Time.deltaTime, 0f, 0f );
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate( -3f*Time.deltaTime, 0f, 0f );
+		}
+		if (Input.GetKey(KeyCode.LeftShift)) {
+			transform.Translate( 0f, 0f, -3f*Time.deltaTime );
+		}
+		if (Input.GetKey(KeyCode.RightShift)) {
+			transform.Translate( 0f, 0f, 3f*Time.deltaTime );
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			transform.Rotate( 0f, 0f, 300f*Time.deltaTime );
+		}
+	}
+}
